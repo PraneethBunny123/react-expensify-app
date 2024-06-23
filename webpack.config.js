32949fd20530d3652a09595c76bf10da476b6230
praneeth <gp.gunturu@gmail.com>
2024-06-23 18:30:28 -0500

    setup production webpack build and removed bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,48 @@
 const pathName = require("path")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-    entry: "./src/app.js",
-    output: {
-        path: pathName.join(__dirname, 'public'),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_module/
-        }, {
-            test: /\.s?css$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }]
-    },
-    devtool: "eval-cheap-module-source-map",
-    devServer: {
-        static: {
-            directory: pathName.join(__dirname, 'public')
+module.exports = (env, argv) => {
+
+    const isProduction = argv.mode === 'production'
+    return {
+        entry: "./src/app.js",
+        output: {
+            path: pathName.join(__dirname, 'public'),
+            filename: "bundle.js"
         },
-        historyApiFallback: true
-    },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_module/
+            }, {
+                test: /\.s?css$/,
+                use: [ MiniCssExtractPlugin.loader, 
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ],
+            }]
+        },
+        plugins: [new MiniCssExtractPlugin({ 
+                    filename: 'styles.css'
+                })
+        ],
+        devtool: isProduction ? 'source-map' : "inline-cheap-module-source-map",
+        devServer: {
+            static: {
+                directory: pathName.join(__dirname, 'public')
+            },
+            historyApiFallback: true
+        }
+    }
 }
